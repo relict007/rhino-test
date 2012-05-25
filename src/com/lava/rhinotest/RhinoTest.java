8e343759b7152d5bc817467a0adf2f7aceecebed
saurabh lakhera <saurabh.lakhera@lavainternational.in>
2012-05-25 20:16:01 +0530

    can launch app

--- src/com/lava/rhinotest/RhinoTest.java
+++ src/com/lava/rhinotest/RhinoTest.java
@@ -20,15 +20,58 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
+import org.mozilla.javascript.*;
+import android.util.Log;
 
 public class RhinoTest extends Activity {
-    private static final String TAG = "NoteEditor";
+	private static Scriptable m_global_scope = null;
+	private Scriptable m_scope;
+	private org.mozilla.javascript.Context cx;
+    private static final String TAG = "RhinoTest";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+	    Log.e(TAG,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+	initGlobalScope();
+	initScriptEngine();
+	exitScriptEngine();
         
     }
+
+
+
+
+
+	private void initGlobalScope()
+	{
+		Log.e(TAG,"initGlobalScope");
+		org.mozilla.javascript.Context tempCx = org.mozilla.javascript.Context.enter();
+		tempCx.setOptimizationLevel(-1);
+		m_global_scope = tempCx.initStandardObjects();
+		org.mozilla.javascript.Context.exit();
+	}
+
+	private void initScriptEngine() 
+	{
+		Log.e(TAG,"initScriptEngine");
+		cx = org.mozilla.javascript.Context.enter();
+		cx.setOptimizationLevel(-1);
+		m_scope = cx.newObject(m_global_scope);
+		m_scope.setPrototype(m_global_scope);
+		m_scope.setParentScope(null);
+
+	}
+
+
+
+
+
+	private void exitScriptEngine()
+	{
+		Log.e(TAG,"exitScriptEngine");
+		org.mozilla.javascript.Context.exit();
+	}
 }
