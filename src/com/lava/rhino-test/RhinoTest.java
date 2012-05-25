1826ef2333aa3dca86584583001d44f87e09fa6d
saurabh lakhera <saurabh.lakhera@lavainternational.in>
2012-05-25 19:55:09 +0530

    created a dummy activity

---
+++ src/com/lava/rhino-test/RhinoTest.java
@@ -0,0 +1,34 @@
+
+package com.lava.rhino-test;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RhinoTest extends Activity {
+    private static final String TAG = "NoteEditor";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.default);
+        
+    }
+}
